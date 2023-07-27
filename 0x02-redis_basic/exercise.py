ab552f194277cb352a50ecca70600b60110015e3
Bolexzy <boluoyewumi79@gmail.com>
2023-07-27 10:26:49 +0100

    Update: Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Task 0. Writing strings to Redis
 """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -22,3 +22,21 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        '''Retrieves a value from a Redis data storage.
+        '''
+        data = self._redis.get(key)
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> str:
+        '''Retrieves a string value from a Redis data storage.
+        '''
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        '''Retrieves an int value from a Redis data storage.
+        '''
+        return self.get(key, int)
